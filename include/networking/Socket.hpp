9c61d157ba0cec853079daef92d43817afcbbdb8
FlaviaV <flavia.voicuu@gmail.com>
2024-07-19 17:35:12 +0200

    Add incomplete implentation of the Accept Method

--- include/networking/Socket.hpp
+++ include/networking/Socket.hpp
@@ -16,12 +16,14 @@ public:
 
   bool Bind(int port, const std::string &address);
   bool Listen(int backlog);
+  Socket Accept();
 
   int getFd() const;
 
 private:
   int fd_;
-  struct sockaddr_in address_;
+  struct sockaddr_in address_;       // Server addess
+  struct sockaddr_in clientAddress_; // Client address
 };
 
 } // namespace net
