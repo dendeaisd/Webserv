10fc635dac6fb891d765c800ef71054bd22ca588
Fritz Gäbler <fritzlmgaebler@gmail.com>
2024-08-12 10:40:16 +0200

    add states for parsing

---
+++ include/configFile/states.hpp
@@ -0,0 +1,11 @@
+#ifndef STATES_HPP
+#define STATES_HPP
+
+typedef enum EStoringStates {
+  MAIN_CONTEXT = 0,
+  HTTPS_CONTEXT = 1,
+  SERVER_CONTEXT_IN_HTTP = 2,
+  LOCATION_CONTEXT_IN_SERVER = 3
+} TStoringStates;
+
+#define  // STATES_HPP
