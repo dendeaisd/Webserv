d15b79b42390672344856ee43262c563f7b1ca94
dendeaisd <84511148+dendeaisd@users.noreply.github.com>
2024-08-26 14:24:38 +0200

    Revert "merge main into 37-error-pages"
    This reverts commit dda87f3c95b821f05caeb915169a5ead7780c916.

---
+++ src/networking/PollManager.cpp
@@ -0,0 +1,47 @@
+#include "../../include/networking/PollManager.hpp"
+
+#include <errno.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include <algorithm>
+#include <cerrno>
+#include <cstddef>
+#include <cstring>
+#include <vector>
+
+using namespace net;
+
+PollManager::PollManager() {}
+
+void PollManager::addSocket(int fd, int events) {
+  struct pollfd pfd;
+  pfd.fd = fd;
+  pfd.events = events;
+  pfd.revents = 0;
+  fds_.push_back(pfd);
+}
+
+void PollManager::removeSocket(int fd) {
+  if (fd < 0) {
+    throw invalidFd(fd);
+  }
+
+  for (size_t i = 0; i < fds_.size(); ++i) {
+    if (fds_[i].fd == fd) {
+      std::swap(fds_[i], fds_.back());
+      fds_.pop_back();
+      break;
+    }
+  }
+}
+
+void PollManager::pollSockets() {
+  int poll_count = poll(&fds_[0], fds_.size(), -1);
+  if (poll_count < 0) {
+    throw pollFailed(std::strerror(errno));
+  }
+}
+
+std::vector<struct pollfd>& PollManager::getFds() { return fds_; }
