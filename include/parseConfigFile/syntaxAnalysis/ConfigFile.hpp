d86ec9d512403c2df5ef151aa059ce70b4736ef2
fritz-gabler <fritzlmgaebler@gmail.com>
2024-09-11 17:28:51 +0200

    clear old syntax Analysis to be able to parse tokens not the file directly

---
+++ include/parseConfigFile/syntaxAnalysis/ConfigFile.hpp
@@ -0,0 +1,18 @@
+#ifndef CONFIG_FILE
+#define CONFIG_FILE
+
+#include "HttpContext.hpp"
+#include <string>
+
+class ConfigFile {
+ public:
+  ConfigFile();
+  void printConfigFile();
+  std::string _workerProcessesValue;
+  std::string _pidValue;
+  std::string _errorLogValue;
+
+  HttpContext _httpContext;
+};
+
+#endif
--- include/parseConfigFile/syntaxAnalysis/SyntaxAnalysis.hpp
+++ include/parseConfigFile/syntaxAnalysis/SyntaxAnalysis.hpp
@@ -4,14 +4,17 @@
 #include <iostream>
 #include <stack>
 #include <string>
+#include <vector>
+#include <memory>
 
-#include "HttpContext.hpp"
+#include "ConfigFile.hpp"
+#include "NodeToken.hpp"
 
 enum class StoringStates {
   MAIN_CONTEXT = 0,
   HTTPS_CONTEXT = 1,
-  SERVER_CONTEXT_IN_HTTP = 2,
-  LOCATION_CONTEXT_IN_SERVER = 3
+  SERVER_CONTEXT = 2,
+  LOCATION_CONTEXT = 3
 };
 
 enum EBracketStatus {
@@ -23,34 +26,12 @@ enum EBracketStatus {
 
 class SyntaxAnalysis {
  public:
-  SyntaxAnalysis();
 
   void printConfigFileContent();
   void parseConfiguration(const std::string &fileName);
 
-  std::string _workerProcessesValue;
-  std::string _pidValue;
-  std::string _errorLogValue;
-
-  HttpContext _httpContext;
 
  private:
-  void setCurrentState(const std::string &line);
-  void possibleNewServerContextSetup(const std::string &line);
-  void trackBrackets(const std::string &line);
-  EBracketStatus transferStateToBracketStatus();
-  void mainContextSaveDirective(const std::string &line);
-  std::string getValue(const std::string &line);
-  void httpContextSave(const std::string &line);
-  std::string getKey(const std::string &line);
-  void serverContextSave(const std::string &line);
-  void locationContextSave(const std::string &line);
-  void removeWhiteSpacesFront(std::string &str);
-  int numberOfWordsSeparatedBySpaces(const std::string &str);
-  bool isStoringState(const std::string &line, StoringStates state);
-  void getStateAsString(std::string &stateStr, StoringStates &state);
-  void possibleNewLocationSetup(std::string &line);
-
   StoringStates _state;
   std::stack<char> _bracketStatus[4];
 };
