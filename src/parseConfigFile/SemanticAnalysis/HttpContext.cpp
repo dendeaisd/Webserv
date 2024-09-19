385cbe0b955767c21ee36139157f772a57dea5c6
fritz-gabler <fritzlmgaebler@gmail.com>
2024-09-19 20:40:05 +0200

    add correct printing of new types in http structure

--- src/parseConfigFile/SemanticAnalysis/HttpContext.cpp
+++ src/parseConfigFile/SemanticAnalysis/HttpContext.cpp
@@ -6,7 +6,7 @@
 /*   By: fgabler <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 11:37:24 by fgabler           #+#    #+#             */
-/*   Updated: 2024/08/26 17:20:26 by fgabler          ###   ########.fr       */
+/*   Updated: 2024/09/19 17:55:43 by fgabler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,49 +24,17 @@ HttpContext::HttpContext() {
   _limitReqZoneValue.clear();
 }
 
-void HttpContext::addNewEmptyServer() {
-  _serverContext.push_back(std::make_unique<ServerContext>());
-}
-
-void HttpContext::httpSaveDirectiveValue(const std::string &directive,
-                                         const std::string &value) {
-  if (directive == "geoip_country")
-    _geoipCountryValue = value;
-  else if (directive == "proxy_cache_path")
-    _proxyCachePathValue = value;
-  else if (directive == "proxy_cache")
-    _proxyCacheValue = value;
-  else if (directive == "proxy_cache_use_stale")
-    _proxyCacheUseStaleValue = value;
-  else if (directive == "gzip")
-    _gzipValue = value;
-  else if (directive == "gzip_types")
-    _gzipTypesValue = value;
-  else if (directive == "limit_req_zone")
-    _limitReqZoneValue = value;
-  else if (directive == "proxy_set_header")
-    _proxySetHeaderValue.push_back(value);
-  else if (directive == "proxy_cache_valid")
-    _proxyCacheValidValue.push_back(value);
-}
-
-void HttpContext::serverSaveContextOrDirective(const std::string &key,
-                                               const std::string &value) {
-  _serverContext.back()->serverSaveDirectiveValue(key, value);
-}
-
-void HttpContext::printHttpContent() {
-  std::cout << "-Http-\n"
-            << "geo ip country: [" << _geoipCountryValue << "]" << std::endl
-            << "proxy cache path: [" << _proxyCachePathValue << "]" << std::endl
-            << "proxy cache: [" << _proxyCacheValue << "]" << std::endl
-            << "proxy cache use stale: [" << _proxyCacheUseStaleValue << "]"
-            << std::endl
-            << "gzip: [" << _gzipValue << "]" << std::endl
-            << "gzip types: [" << _gzipTypesValue << "]" << std::endl
-            << "limit req zone: [" << _limitReqZoneValue << "]" << std::endl;
-  printVectorOfStrings(_proxySetHeaderValue, "proxy set header");
-  printVectorOfStrings(_proxyCacheValidValue, "proxy cache valid");
+void HttpContext::printHttpContent() const noexcept {
+  std::cout << "-Http-\n";
+  typeFormatPrint("geo ip country", _geoipCountryValue);
+  typeFormatPrint("proxy_cache_path", _proxyCachePathValue);
+  typeFormatPrint("proxy_cache", _proxyCacheValue);
+  typeFormatPrint("gzip", _gzipValue);
+  typeFormatPrint("limit_req_zone", _limitReqZoneValue);
+  printVectorOfStrings(_gzipTypesValue, "gzip_types");
+  printVectorOfStrings(_proxyCacheUseStaleValue, "proxy_cache_use_stale");
+  printVectorOfStrings(_proxySetHeaderValue, "proxy_set_header");
+  printVectorOfStrings(_proxyCacheValidValue, "proxy_cache_valid");
 
   std::cout << "\n";
   auto it = _serverContext.begin();
@@ -77,14 +45,20 @@ void HttpContext::printHttpContent() {
   }
 }
 
-void HttpContext::printVectorOfStrings(const std::vector<std::string> &vec,
-                                       const std::string valueType) const {
+void HttpContext::typeFormatPrint(const std::string &type,
+                                  const std::string &value) const noexcept {
+  std::cout << type << ": [" << value << "]\n";
+}
+
+void HttpContext::printVectorOfStrings(
+    const std::vector<std::string> &vec,
+    const std::string valueType) const noexcept {
   auto it = vec.begin();
   int i;
-
   i = 0;
   while (it != vec.end()) {
-    std::cout << "[" << i << "] " << valueType << ": " << *it << std::endl;
+    std::cout << "[" << i << "] " << valueType << ": " << " "<< *it << std::endl;
     it++;
+    i++;
   }
 }
